4612d1a91fe65d296ea1b5c12e290612a37ab556
kbardsle <kaba6117@colorado.edu>
2024-04-26 14:58:11 -0600

    process 2017 lat long and population data for use in model

---
+++ code/Pop_lat_long_data_wrangling_2017.R
@@ -0,0 +1,63 @@
+# --------------------------------------------------------
+# calculating 3-digit zip population sizes and weighted latitude longitudes for 2017 census data
+# 26 Apr 2024
+# Katie Bardsley
+# --------------------------------------------------------
+
+library(tidyverse)
+
+# read in data and reformat
+latlong_zip <-read.table('data/2017_Gaz_zcta_national.txt',
+                sep='\t',
+                header=TRUE,
+                colClasses = "character")
+      # data from: 
+      # https://www.census.gov/geographies/reference-files/time-series/geo/gazetteer-files.2017.html#list-tab-264479560
+      # see here for more information on column contents (very bottom of page): 
+      # https://www.census.gov/programs-surveys/geography/technical-documentation/records-layout/gaz-record-layouts.2017.html#list-tab-1913338080
+
+pop_zip <- read_csv("data/ACSDP5Y2017.DP05-Data.csv")
+
+pop_zip_simple <- pop_zip %>% slice(-c(1)) %>% separate(col="NAME", into=c("prefix","GEOID")) %>% select(c(GEOID, DP05_0001E))
+colnames(pop_zip_simple)[2] <- "POPULATION"
+
+# combine dataframes
+zipdata_full <- full_join(pop_zip_simple, latlong_zip, by="GEOID")
+
+# convert lat long, pop to numeric
+zipdata_full$INTPTLAT <- as.numeric(zipdata_full$INTPTLAT)
+zipdata_full$INTPTLONG <- as.numeric(zipdata_full$INTPTLONG)
+zipdata_full$POPULATION <- as.numeric(zipdata_full$POPULATION)
+
+
+# add column for 3 digit zip
+zipdata_full <- zipdata_full %>%
+  dplyr::mutate(ZIP3 = substr(as.character(GEOID), 1, 3))
+
+# sum populations for 3-digit zips
+zip3_pops <- zipdata_full %>% group_by(ZIP3) %>% summarize(POP3 = sum(POPULATION))
+
+# combine with full df
+zipdata_full3 <- full_join(zipdata_full, zip3_pops, by="ZIP3")
+
+# filter out zips with less than 20,000 people
+zipdata3_filtered <- zipdata_full3 %>% filter(POP3 >= 20000)
+
+# calculate population-weighted lat/long
+weighted_latlong <- zipdata3_filtered %>% 
+  group_by(ZIP3) %>% 
+  summarize(WEIGHTED_LAT = weighted.mean(x=INTPTLAT, w=POPULATION),
+            WEIGHTED_LONG = weighted.mean(x=INTPTLONG, w=POPULATION))
+
+# combine with full df
+zipdata3_weighted_df <- full_join(zipdata3_filtered, weighted_latlong, by="ZIP3")
+
+# simplified df with just 3-digit areas
+df_3digit <- full_join(zip3_pops, weighted_latlong, by="ZIP3")
+df_3digit_filtered <- df_3digit %>% filter(POP3 >= 20000)
+
+# filter for contiguous US
+df_US <- df_3digit_filtered %>% filter(WEIGHTED_LONG > -126 & WEIGHTED_LONG < -65) %>% filter(WEIGHTED_LAT > 23 & WEIGHTED_LAT < 50)
+
+# save as csv
+write.csv(df_US, "data/2017_pop_lat_long_data.csv", row.names=FALSE)
